cd14b9e9757e469a58cc1a5b56a90194b4371c35
Farhaa <farha.mansuri@iviewlabs.net>
2022-04-13 12:19:00 +0530

    Boilerplate

---
+++ src/main/java/com/formbook/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.formbook.repository;
+
+import com.formbook.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/formbook/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.formbook.service.dto;
